436aae5fde2482fd751fb254f1fa09e0faf940c3
岡本紘治 <>
2024-09-16 01:01:22 +0900

    使用しないimportを削除

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState, useEffect } from 'react'
-import Link from 'next/link'
 import { usePathname, useRouter } from 'next/navigation'
 import { FaFlag, FaFlagUsa, FaBars } from 'react-icons/fa'
 
