b7cd6be4b8ef36b3ab52d0564bac50939ea0c298
岡本紘治 <>
2024-09-16 00:49:47 +0900

    設定ファイルを追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,21 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'magic-blue': '#1C305C',
+        'magic-white': '#F3F3F2',
+        'magic-silver': '#C0C0C0',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+
+export default config
